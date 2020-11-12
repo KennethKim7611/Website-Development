c40deea10acfef4058b7f66d3d8f5529f00262b5
KennethKim7611 <71912350+KennethKim7611@users.noreply.github.com>
2020-11-13 02:29:57 +0900

    Initial commit

---
+++ js/terminal.js
@@ -0,0 +1,59 @@
+$(function() {
+  var data = [
+    {
+      action: 'type',
+      strings: ["about me"],
+      output: 'Kenneth Kim / University of Texas at Austin 2023<br>Chemical Engineering<br><br>',
+      postDelay: 1000
+    },
+  { 
+    action: 'type',
+    strings: ["Read my editorials :) ", 'Select language to enter!!'],
+    postDelay: 1000
+  },
+  
+];
+  runScripts(data, 0);
+});
+
+function runScripts(data, pos) {
+    var prompt = $('.prompt'),
+        script = data[pos];
+    if(script.clear === true) {
+      postDelay:5000,
+      $('.history').html(''); 
+    }
+    switch(script.action) {
+        case 'type':
+          // cleanup for next execution
+          prompt.removeData();
+          $('.typed-cursor').text('');
+          prompt.typed({
+            strings: script.strings,
+            typeSpeed: 50,
+            callback: function() {
+              var history = $('.history').html();
+              history = history ? [history] : [];
+              history.push('$ ' + prompt.text());
+              if(script.output) {
+                history.push(script.output);
+                prompt.html('');
+                $('.history').html(history.join('<br>'));
+              }
+              // scroll to bottom of screen
+              $('section.terminal').scrollTop($('section.terminal').height());
+              // Run next script
+              pos++;
+              if(pos < data.length) {
+                setTimeout(function() {
+                  runScripts(data, pos);
+                }, script.postDelay || 1000);
+              }
+            }
+          });
+          break;
+        case 'view':
+
+          break;
+    }
+}
